e3925a53d21d4184c00280230ff7e0c10562e4e6
Pragati Rana <shalu.pragati11@gmail.com>
2020-05-13 13:27:27 +0530

    Initial commit.

--- tracker/tracker/spiders/daily.py
+++ tracker/tracker/spiders/daily.py
@@ -3,13 +3,13 @@ from ..items import TrackerItem
 class DailyUpdate(scrapy.Spider):
     name = 'track'
     start_urls = [
-        'https://api.covid19india.org/'
+        'https://api.covid19india.org/csv/'
     ]
     def parse(self, response):
 
         items = TrackerItem()
 
-        all_div =  response.css(".highlighter-rouge:nth-child(7)::text").extract()
+        all_div =  response.css("tr:nth-child(1) td:nth-child(3)::text").extract()
 
         items['all_div'] = all_div
         yield items
